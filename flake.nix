1444e63b6e27e4133f61ab0c8b97afbecc2d2059
Vilhelm Bergsoe <vilhelmbergsoe@gmail.com>
2022-07-06 19:15:44 +0200

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
-
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShell.x86_64-linux =
-        pkgs.mkShell { buildInputs = [ pkgs.pandoc pkgs.entr pkgs.gnumake pkgs.texlive.combined.scheme-small ]; };
-   };
+  description = "pandoc template";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      devShell = pkgs.mkShell { buildInputs = [ pkgs.pandoc pkgs.entr pkgs.gnumake pkgs.texlive.combined.scheme-small ]; };
+    });
 }
